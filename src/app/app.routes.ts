db0e7900b8862b4b463bc7c8965b82432cd3d332
Sweshika <sweshisj@gmail.com>
2025-04-25 22:44:45 +1000

    added routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { GamesComponent } from './games/games.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "home", component: HomeComponent },
+    { path: "projects", component: ProjectsComponent },
+    { path: "games", component: GamesComponent }
+];
