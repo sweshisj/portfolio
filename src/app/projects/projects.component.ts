02483a17205853f5d4ed37c629a698609060b295
Sweshika <sweshisj@gmail.com>
2025-04-25 22:54:40 +1000

    basic UI

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,11 +1,17 @@
+import { CommonModule, NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-projects',
-  imports: [],
+  imports: [CommonModule, NgFor],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
 export class ProjectsComponent {
+  projects = [
+    { name: "phaser", description: "developed 2D games" }
+  ]
+  showProjectDetails(project: any) {
 
+  }
 }
