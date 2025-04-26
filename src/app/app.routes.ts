87945d4e854322ac780bd84a622566caa1739472
Sweshika <sweshisj@gmail.com>
2025-04-26 21:28:16 +1000

    Implement lazy loading for each route to improve performance.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,9 +8,9 @@ import { NumberGuessComponent } from './games/number-guess/number-guess.componen
 
 export const routes: Routes = [
     { path: "", component: HomeComponent },
-    { path: "projects", component: ProjectsComponent },
+    { path: "projects", loadComponent: () => import('./projects/projects.component').then(c => c.ProjectsComponent) },
     { path: "project-details/:id", component: ProjectDetailsComponent },
-    { path: "games", component: GamesComponent },
-    { path: "tic-tac-toe", component: TicTacToeComponent },
-    { path: "number-guess", component: NumberGuessComponent }
+    { path: "games", loadComponent: () => import('./games/games.component').then(c => c.GamesComponent) },
+    { path: "tic-tac-toe", loadComponent: () => import('./games/tic-tac-toe/tic-tac-toe.component').then(c => c.TicTacToeComponent) },
+    { path: "number-guess", loadComponent: () => import('./games/number-guess/number-guess.component').then(c => c.NumberGuessComponent) }
 ];
